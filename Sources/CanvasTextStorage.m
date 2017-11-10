7fd1929f5a5e04f5cf8cd320e4b7aa6a1e107577
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2017-11-10 17:46:07 -0500

    Fix the errored setAttributes range calls (from calls that inserted text that was then 'consumed' by the markdown processing code into something shorter in range than the original text.  This was a tough one to track down.

--- Sources/CanvasTextStorage.m
+++ Sources/CanvasTextStorage.m
@@ -10,6 +10,7 @@
 
 @interface CanvasTextStorage ()
 @property (nonatomic) NSUInteger editCount;
+@property (nonatomic) BOOL isReplacing;
 @end
 
 @implementation CanvasTextStorage
@@ -18,6 +19,7 @@
 
 @synthesize editCount = _editCount;
 @synthesize canvasDelegate = _canvasDelegate;
+@synthesize isReplacing = _isReplacing;
 
 - (BOOL)isEditing {
 	return self.editCount > 0;
@@ -35,6 +37,27 @@
 	[super endEditing];
 	self.editCount -= 1;
 }
+
+/* It appears that the normal sequence in the call chain calls replaceString then setAttributes.  But when we are handling attributes, we need to get block that call, since sometimes it has the wrong range (when a markdown command shrinks the total text, for instance.  So we override this function and the next.  This one puts up a barrier and the other honors the barrier to avoid the errored setAttributes call */
+- (void) replaceCharactersInRange:(NSRange)range withAttributedString:(NSAttributedString *)attrString {
+    self.isReplacing = true;
+    [super replaceCharactersInRange:range withAttributedString: attrString];
+    self.isReplacing = false;
+    // Block call for set attributed after this
+}
+
+- (void)setAttributes:(NSDictionary<NSString *,id> *)attributes range:(NSRange)range {
+    if (self.isReplacing) {
+        return;
+    } // See linkage to code above
+    
+    if (NSMaxRange(range) > self.length) {
+        NSLog(@"WARNING: Tried to set attributes at out of bounds range %@. Length: %lu", NSStringFromRange(range), (unsigned long)self.length);
+        return;
+    }
+    [super setAttributes: attributes range: range];
+}
+
 
 - (void)replaceCharactersInRange:(NSRange)range withString:(NSString *)aString {
 	// Local changes are delegated to the text controller
