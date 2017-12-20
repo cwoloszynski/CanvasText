9e3338495e63cda401fc5a6524d8a2440ebd1bda
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2017-12-20 15:16:13 -0500

    Working system-wide unit testing in place

--- Tests/SelectionControllerTests.swift
+++ Tests/SelectionControllerTests.swift
@@ -165,7 +165,7 @@ class SelectionControllerTests: XCTestCase {
 	func testInvalid() {
 		let output = SelectionController.adjust(
 			selection: startingSelection,
-			replacementRange: .zero,
+			replacementRange: NSRange(location: 0, length: 0),
 			replacementLength: 0
 		)
 		XCTAssertEqual(startingSelection, output)
